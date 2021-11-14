97ae025a75978198c2ee0f879245440687d1dc48
Daniel Lisko <djlisko01@Daniels-Air.hsd1.ca.comcast.net>
2021-11-14 13:59:54 -0800

    Started databsae: Can add users via signups (includes user exist check)

---
+++ database/dbManager.js
@@ -0,0 +1,83 @@
+const { MongoClient } = require("mongodb");
+
+//This connects to the collections
+const collectionConnect = async (documents) => {
+  //Database Name
+  const dbName = "laundryApp";
+
+  //Connect to url
+  const url = "mongodb://localhost:27017";
+  const client = await new MongoClient(
+    url,
+    { useUnifiedTopology: true },
+    { useNewUrlParser: true }
+  );
+
+  //Connect to db
+  await client.connect();
+  console.log("Connected to server");
+
+  //Connect to collection
+  const db = await client.db(dbName);
+  collection = await db.collection(documents);
+  console.log("collection => ", documents);
+
+  return { collection, client };
+};
+
+//-----------------Customer/Provider Shared Features-------//
+
+//Sign Up
+
+const addUser = async (collectionName, data) => {
+  let connectedCollection;
+
+  try {
+    connectedCollection = await collectionConnect(collectionName);
+    const collection = connectedCollection.collection;
+
+    //Check if user Exist: If it does than return false
+    const res = await findUser("loginCredentials", testData.userName);
+
+    if (res) {
+      console.log("User Exist");
+      return false;
+    }
+
+    await collection.insertOne(data);
+    console.log("User Added!");
+  } finally {
+    await connectedCollection.client.close();
+  }
+};
+
+const findUser = async (collectionName, userName) => {
+  try {
+    connectedCollection = await collectionConnect(collectionName);
+    const collection = connectedCollection.collection;
+    const res = await collection.findOne({ userName: userName });
+    return res;
+  } finally {
+    await connectedCollection.client.close();
+  }
+};
+
+//-----------------Provider DB Manager--------------------//
+
+//----------------------Test------------------------------//
+
+const testData = {
+  firstName: "Daniel",
+  lastName: "Lisko",
+  userName: "djlisko01",
+  userType: "provider",
+  password: 123,
+};
+
+main = async () => {
+  // const res = await findUser("loginCredentials", testData.userName);
+  // console.log("---->", res);
+  await addUser("loginCredentials", testData);
+};
+
+main();
--- src/components/signups/SignUpProviderForm.js
+++ src/components/signups/SignUpProviderForm.js
@@ -6,65 +6,74 @@ const SignUpProviderForm = () => {
     <Card>
       <form action="">
         <h2>Contact Info</h2>
+        <input type="hidden" name="userType" value="provider" />
         <div class="fullLengthIn">
-          <label htmlFor="">Company Name</label>
-          <input type="text" placeholder="Enter Company Name" />
+          <label htmlFor="companyName">Company Name</label>
+          <input
+            type="text"
+            name="companyName"
+            placeholder="Enter Company Name"
+          />
         </div>
         <div>
           <div className="inline">
-            <label htmlFor="">First Name</label>
-            <input type="text" placeholder="Enter First Name" />
+            <label htmlFor="firstName">First Name</label>
+            <input
+              type="text"
+              name="firstName"
+              placeholder="Enter First Name"
+            />
           </div>
           <div className="inline">
-            <label htmlFor="">Last Name</label>
-            <input type="text" placeholder="Enter Last Name" />
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" name="lastName" placeholder="Enter Last Name" />
           </div>
         </div>
         <div>
           <div className="inline">
-            <label htmlFor="">Email</label>
-            <input type="email" placeholder="Enter Email" />
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="Enter Email" />
           </div>
           <div className="inline">
-            <label htmlFor="">Phone</label>
-            <input type="tel" placeholder="XXX-XXX-XXXX" />
+            <label htmlFor="phone">Phone</label>
+            <input type="tel" name="phone" placeholder="XXX-XXX-XXXX" />
           </div>
         </div>
         <h2>Address</h2>
         <div>
           <div class="fullLengthIn">
-            <label htmlFor="">Address Line 1</label>
-            <input type="text" />
-            <label htmlFor="">Address Line 2</label>
-            <input type="text" />
+            <label htmlFor="address1">Address Line 1</label>
+            <input name="address1" type="text" />
+            <label htmlFor="address2">Address Line 2</label>
+            <input name="address2" type="text" />
           </div>
 
           <div className="inline">
-            <label htmlFor="">City</label>
-            <input type="text" />
+            <label htmlFor="city">City</label>
+            <input name="city" type="text" />
           </div>
           <div className="inline">
-            <label htmlFor="">State</label>
-            <input type="text" />
+            <label htmlFor="state">State</label>
+            <input name="state" type="text" />
           </div>
           <div>
-            <label htmlFor="">Zip Code</label>
+            <label htmlFor="zipCode">Zip Code</label>
           </div>
-          <input type="number" />
+          <input name="zipCode" type="number" />
         </div>
         <h2>User Name & Password</h2>
         <div>
-          <label htmlFor="">User Name</label>
-          <input type="text" />
+          <label htmlFor="userName">User Name</label>
+          <input name="userName" type="text" />
         </div>
         <div>
           <div className="inline">
-            <label htmlFor="">Password</label>
-            <input type="password" />
+            <label htmlFor="password">Password</label>
+            <input name="password" type="password" />
           </div>
           <div className="inline">
-            <label htmlFor="">Confirm Password</label>
-            <input type="password" />
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input name="confirmPassword" type="password" />
           </div>
         </div>
         <div className="flex-parent jc-center">
