606dc4659b8f7816971aaa920d572cce32290f48
michaelchang106 <71532509+michaelchang106@users.noreply.github.com>
2021-11-15 16:52:01 -0800

    fixed useRef

--- src/components/signups/SignUpCustomerForm.js
+++ src/components/signups/SignUpCustomerForm.js
@@ -46,11 +46,11 @@ function SignUpCustomerForm(props) {
       <form className={classes.form} onSubmit={customerSubmitHandler}>
         <div className={classes.control}>
           <label htmlFor="firstname">First Name</label>
-          <input type="text" placeholder="First Name" required id="firstname" />
+          <input type="text" placeholder="First Name" required id={firstNameRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="lastname">Last Name</label>
-          <input type="text" placeholder="Last Name" required id="lastname" />
+          <input type="text" placeholder="Last Name" required id={lastNameRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="email">E-mail</label>
@@ -58,7 +58,7 @@ function SignUpCustomerForm(props) {
             type="email"
             placeholder="email@email.com"
             required
-            id="email"
+            id={emailRef}
           />
         </div>
         <div className={classes.control}>
@@ -67,16 +67,16 @@ function SignUpCustomerForm(props) {
             type="password"
             placeholder="Password"
             required
-            id="password"
+            id={passwordRef}
           />
         </div>
         <div className={classes.control}>
           <label htmlFor="city">City</label>
-          <input type="text" placeholder="City Name" required id="city" />
+          <input type="text" placeholder="City Name" required id={cityRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor="state">State</label>
-          <input type="text" placeholder="State Name" required id="state" />
+          <input type="text" placeholder="State Name" required id={stateRef}  />
         </div>
         <div className={classes.control}>
           <label htmlFor="phone">Phone Number</label>
@@ -85,7 +85,7 @@ function SignUpCustomerForm(props) {
             pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
             placeholder="123-456-7890"
             required
-            id="phone"
+            id={phoneNumberRef} 
           />
         </div>
         <div className={classes.actions}>
