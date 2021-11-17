a2e41e0a9d5049f07eebd6cf59d1015166091c08
Daniel Lisko <djlisko01@Daniels-Air.hsd1.ca.comcast.net>
2021-11-16 16:48:45 -0800

    Added useState to Provider Signup

--- front/src/components/signups/SignUpProviderForm.js
+++ front/src/components/signups/SignUpProviderForm.js
@@ -1,36 +1,52 @@
 import Card from "../ui/Card";
 import "./SignUpProvide.css";
-// import { useState } from "react";
+import { useState } from "react";
 
-const SignUpProviderForm = () => {
-  //Which is the initial state, and returns two values:
-  //1) the current state and a 2)function that can be used to update the state.
+const SignUpProviderForm = ({ postProviderData }) => {
+  const [providerInfo, setProviderInfo] = useState({
+    companyName: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    address1: "",
+    address2: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-  //Sets the user info w/o password
-  // let [userInfo, setUserInfo] = useState({
-  //   companyName: "",
-  //   firstName: "",
-  //   lastName: "",
-  //   address: "",
-  //   city: "",
-  //   phone: "",
-  //   zipCode: null,
-  //   userName: "",
-  // });
+  console.log("provider", providerInfo);
 
-  //Sets user login credentials (password, userType, userName)
-  // let [loginCredential, setloginCredential] = useState();
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setProviderInfo({ ...providerInfo, [name]: value });
+  };
+
+  //Hanle submit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(event.target[0].value);
+  };
 
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Contact Info</h2>
         <div className="fullLengthIn">
           <label htmlFor="companyName">Company Name</label>
           <input
             type="text"
             name="companyName"
+            id="companyName"
             placeholder="Enter Company Name"
+            value={providerInfo.companyName}
+            onChange={handleChange}
           />
         </div>
         <div>
@@ -39,26 +55,43 @@ const SignUpProviderForm = () => {
             <input
               type="text"
               name="firstName"
+              value={providerInfo.firstName}
+              id="firstName"
               placeholder="Enter First Name"
+              onChange={handleChange}
             />
           </div>
           <div className="inline">
             <label htmlFor="lastName">Last Name</label>
-            <input type="text" name="lastName" placeholder="Enter Last Name" />
+            <input
+              type="text"
+              name="lastName"
+              value={providerInfo.lastName}
+              id="lastName"
+              placeholder="Enter Last Name"
+              onChange={handleChange}
+            />
           </div>
         </div>
         <div>
           <div className="inline">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" placeholder="Enter Email" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter Email"
+              onChange={handleChange}
+            />
           </div>
           <div className="inline">
-            <label htmlFor="phone">Phone</label>
+            <label htmlFor="phoneNumber">Phone</label>
             <input
               type="tel"
-              name="phone"
+              name="phoneNumber"
+              id="phoneNumber"
               pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
               placeholder="XXX-XXX-XXXX"
+              onChange={handleChange}
             />
           </div>
         </div>
@@ -66,37 +99,64 @@ const SignUpProviderForm = () => {
         <div>
           <div className="fullLengthIn">
             <label htmlFor="address1">Address Line 1</label>
-            <input name="address1" type="text" />
+            <input
+              name="address1"
+              id="address1"
+              type="text"
+              onChange={handleChange}
+            />
             <label htmlFor="address2">Address Line 2</label>
-            <input name="address2" type="text" />
+            <input
+              name="address2"
+              id="address2"
+              type="text"
+              onChange={handleChange}
+            />
           </div>
 
           <div className="inline">
             <label htmlFor="city">City</label>
-            <input name="city" type="text" />
+            <input name="city" id="city" type="text" onChange={handleChange} />
           </div>
           <div className="inline">
             <label htmlFor="state">State</label>
-            <input name="state" type="text" />
+            <input
+              name="state"
+              id="state"
+              type="text"
+              onChange={handleChange}
+            />
           </div>
           <div>
             <label htmlFor="zipCode">Zip Code</label>
           </div>
-          <input name="zipCode" type="number" />
+          <input
+            name="zipCode"
+            id="zipCode"
+            type="number"
+            onChange={handleChange}
+          />
         </div>
-        <h2>User Name & Password</h2>
-        <div>
-          <label htmlFor="userName">User Name</label>
-          <input name="userName" type="text" />
-        </div>
+        <h2>Password</h2>
+
         <div>
           <div className="inline">
             <label htmlFor="password">Password</label>
-            <input name="password" type="password" />
+            <input
+              name="password"
+              id="password"
+              type="password"
+              onChange={handleChange}
+            />
           </div>
           <div className="inline">
             <label htmlFor="confirmPassword">Confirm Password</label>
-            <input name="confirmPassword" type="password" />
+            <input
+              name="confirmPassword"
+              id="confirmPassword"
+              type="password"
+              onChange={handleChange}
+            />
           </div>
         </div>
         <div className="flex-parent jc-center">
--- front/src/pages/SignUpProviders.js
+++ front/src/pages/SignUpProviders.js
@@ -1,9 +1,15 @@
 import ProviderForm from "../components/signups/SignUpProviderForm";
+
 const SignUpProviders = () => {
+  const submitFormHandle = (event) => {
+    event.preventDefault();
+    console.log(event);
+  };
+
   return (
     <div>
       <h1>Laundry Service Provider Sign Up</h1>
-      <ProviderForm />
+      <ProviderForm submitHandle={submitFormHandle} />
     </div>
   );
 };
