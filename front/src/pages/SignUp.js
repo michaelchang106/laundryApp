00014b297eb4e0599d638809bad69275c540a69d
michaelchang106 <71532509+michaelchang106@users.noreply.github.com>
2021-11-22 17:36:41 -0800

    changed signup message

--- front/src/pages/SignUp.js
+++ front/src/pages/SignUp.js
@@ -1,17 +1,32 @@
 import { Link } from "react-router-dom";
+import Card from "../components/ui/Card";
 
 function SignUp() {
   return (
     <div>
       <h1>Sign Up Page</h1>
-      <ul>
-        <li key="signUpProviders">
-          <Link to="/signUpProviders">Providers Sign Up</Link>
-        </li>
-        <li key="signUpCustomers">
-          <Link to="/signUpCustomers">Customers Sign Up</Link>
-        </li>
-      </ul>
+      <div className="row">
+        <div className="col-6 d-flex justify-content-center">
+          <Card>
+            <div className="d-flex justify-content-center">
+              <h3>I am a laundry provider! I can wash!</h3>
+            </div>
+            <div className="d-flex justify-content-center">
+              <Link to="/signUpProviders">Providers Sign Up</Link>
+            </div>
+          </Card>
+        </div>
+        <div className="col-6 ">
+          <Card>
+            <div className="d-flex justify-content-center">
+              <h3>I like my laundry clean and spotless!</h3>
+            </div>
+            <div className="d-flex justify-content-center">
+              <Link to="/signUpCustomers">Customers Sign Up</Link>
+            </div>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
